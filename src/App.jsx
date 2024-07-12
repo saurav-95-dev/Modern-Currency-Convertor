4b8e6dc607f4f76b14ff0bc0d6f451bb790ab036
Saurabh <devsaurabh086@gmail.com>
2024-07-12 20:23:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ConverterForm from "./components/ConverterForm";
+
+const App = () => {
+  return (
+    <div className="currency-converter">
+      <h2 className="converter-title">CURRENCY CONVERTOR</h2>
+      <ConverterForm />
+    </div>
+  )
+}
+
+export default App;
